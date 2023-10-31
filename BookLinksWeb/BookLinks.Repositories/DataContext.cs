f6e503854584e380a138a919b81730c5781736bf
kirill <kirillbackend@gmail.com>
2023-10-31 17:16:55 +0300

    Add addBooks

---
+++ BookLinksWeb/BookLinks.Repositories/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using BookLinks.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BookLinks.Repositories
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+            
+        public DbSet<Book> Books { get; set; }
+                
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_configuration.GetConnectionString("WebApiDatabase"));
+        }
+    }
+}
--- BookLinksWeb/BookLinksWeb/Program.cs
+++ BookLinksWeb/BookLinksWeb/Program.cs
@@ -1,18 +1,20 @@
+using BookLinks.Repositories;
 using BookLinks.Service.Services;
 using BookLinks.Service.Services.Interface;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("WebApiDatabase")));
 builder.Services.AddTransient<IBookService, BookService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
